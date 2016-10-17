b1949b6385389cbd31043fa69d06101384b543b3
Missy Allan <missyallan@Melissas-MacBook-Air.local>
2016-10-17 16:34:55 -0400

    animated validator

--- animated-validator-swift/ValidatorViewController.swift
+++ animated-validator-swift/ValidatorViewController.swift
@@ -16,11 +16,15 @@ class ValidatorViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var submitButton: UIButton!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var passwordConfirmTextField: UITextField!
+    
+    var fieldCounter: Int = 0
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        // Do any additional setup after loading the view, typically from a nib.
+        
         self.submitButton.accessibilityLabel = Constants.SUBMITBUTTON
         self.emailTextField.accessibilityLabel = Constants.EMAILTEXTFIELD
         self.emailConfirmationTextField.accessibilityLabel = Constants.EMAILCONFIRMTEXTFIELD
@@ -31,4 +35,121 @@ class ValidatorViewController: UIViewController, UITextFieldDelegate {
         self.submitButton.isEnabled = false
     }
 
+    
+    
+    
+    @IBAction func emailEditEnded(_ sender: AnyObject) {
+        
+        if (emailTextField.text?.contains("@"))! && (emailTextField.text?.contains("."))! {
+              print("Valid email")
+              fieldCounter += 1
+             self.emailTextField.backgroundColor = UIColor.green
+            submitButtonMove()
+        }else{
+           print("Invalid email")
+        UIView.animate(withDuration: 0.4, delay: 0.0, options: [UIViewAnimationOptions.curveLinear,UIViewAnimationOptions.autoreverse], animations: { self.emailTextField.backgroundColor = UIColor.red },
+            completion: nil)
+            
+        
+            
+            
+    }
 }
+    
+    @IBAction func confirmEmailEditEnded(_ sender: AnyObject) {
+        
+        if emailConfirmationTextField.text == emailTextField.text {
+            print("Valid.")
+            fieldCounter += 1
+            self.emailConfirmationTextField.backgroundColor = UIColor.green
+            submitButtonMove()
+        } else {
+            print("Your emails don't match.")
+            UIView.animate(withDuration: 0.4, delay: 0.0, options: [UIViewAnimationOptions.curveLinear,UIViewAnimationOptions.autoreverse], animations: { self.emailConfirmationTextField.backgroundColor = UIColor.red },
+                           completion: nil)
+        }
+    }
+    
+    
+    
+    @IBAction func phoneEditEnded(_ sender: AnyObject) {
+        
+     let phoneNumber = phoneTextField.text
+        
+        if Int(phoneNumber!) != nil && ((phoneTextField.text?.characters.count)! >= 7) {
+            print("Valid number")
+            fieldCounter += 1
+            self.phoneTextField.backgroundColor = UIColor.green
+            submitButtonMove()
+        } else {
+            print ("Not valid number. Try again.")
+            UIView.animate(withDuration: 0.4, delay: 0.0, options: [UIViewAnimationOptions.curveLinear,UIViewAnimationOptions.autoreverse], animations: { self.phoneTextField.backgroundColor = UIColor.red },
+                           completion: nil)
+        }
+        
+    }
+    
+
+    
+    @IBAction func passwordEditEnded(_ sender: AnyObject) {
+        
+        if (passwordTextField.text?.characters.count)! >= 6 {
+            print("Valid password")
+            fieldCounter += 1
+            self.passwordTextField.backgroundColor = UIColor.green
+            submitButtonMove()
+        } else {
+            print("Invalid. Password must be at least 6 characters.")
+            UIView.animate(withDuration: 0.4, delay: 0.0, options: [UIViewAnimationOptions.curveLinear,UIViewAnimationOptions.autoreverse], animations: { self.passwordTextField.backgroundColor = UIColor.red },
+                           completion: nil)
+            
+        }
+    }
+    
+    
+    
+    @IBAction func confirmPasswordEditEnded(_ sender: AnyObject) {
+        
+        if (passwordConfirmTextField.text == passwordTextField.text) {
+            print("Passwords match")
+            fieldCounter += 1
+            self.passwordConfirmTextField.backgroundColor = UIColor.green
+            submitButtonMove()
+        } else {
+            print("Passwords don't match")
+            UIView.animate(withDuration: 0.4, delay: 0.0, options: [UIViewAnimationOptions.curveLinear,UIViewAnimationOptions.autoreverse], animations: { self.passwordConfirmTextField.backgroundColor = UIColor.red },
+            completion: nil)
+            
+        }
+        
+    }
+    
+    
+    func submitButtonMove() {
+        
+        if fieldCounter >= 5 {
+            submitButton.isEnabled = true
+        }
+        
+        UIView.animate(withDuration: 0.4, animations: {
+            
+            self.submitButton.frame.origin.y = -250
+        })
+    
+        
+    }
+    
+        
+        
+}
+
+   
+        
+    
+    
+    
+    
+    
+    
+    
+
